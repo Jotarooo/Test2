cef63a5c122ffe349de2d570ed52cd6cf0d8f4bf
jotarooo <merrylmaitihe@gmail.com>
2021-04-30 09:34:31 -0930

    cacs 2

--- app/src/main/java/com/example/test2/ChiffreAct.java
+++ app/src/main/java/com/example/test2/ChiffreAct.java
@@ -1,4 +1,16 @@
 package com.example.test2;
 
-public class ChiffreAct {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChiffreAct extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chiffreact);
+    }
+
+
+
 }
