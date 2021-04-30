eec8026edab4ae4f6cd865dd1e1cbd69a9a9d773
Pothier-Hiva <markizent@gmail.com>
2021-04-30 12:10:48 -1000

    page ChiffreAct teste audio

--- app/src/main/java/com/example/test2/ChiffreAct.java
+++ app/src/main/java/com/example/test2/ChiffreAct.java
@@ -1,14 +1,32 @@
 package com.example.test2;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ChiffreAct extends AppCompatActivity {
+
+    private ImageButton imageButton11;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chiffreact);
+
+        imageButton11 = findViewById(R.id.imageButton11);
+        final MediaPlayer mediaplayer = MediaPlayer.create(this, R.raw.sample);
+
+        imageButton11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mediaplayer.start();
+            }
+        });
     }
 
 
