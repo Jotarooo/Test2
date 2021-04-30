e7e8357c49cf7c3b6354ee194a7750ec2c807511
jotarooo <merrylmaitihe@gmail.com>
2021-04-30 10:36:08 -0930

    link inter pages SHEEEH Test de nombres boutton 3

--- app/src/main/java/com/example/test2/MainActivity.java
+++ app/src/main/java/com/example/test2/MainActivity.java
@@ -3,15 +3,26 @@ package com.example.test2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button play;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.play = findViewById(R.id.button3);
+        play.setOnClickListener(view -> {
+            Intent otheractivity = new Intent (getApplicationContext(),NombreActivity.class);
+            startActivity(otheractivity);
+            finish();
+        });
     }
+
 
     public void goToAct(View view) {
         Intent intent = new Intent(getApplicationContext(), ChiffreAct.class);
